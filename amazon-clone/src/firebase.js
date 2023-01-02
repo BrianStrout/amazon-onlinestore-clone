e4234dabb3983776389457d37467a4fdca1cf9ef
Brian J. Strout <brianjstrout@gmail.com>
2023-01-02 12:08:32 -0500

    set up create react app, added header component, added firebase, add Material UI

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDN6bpW-Mi4qsdEY7LCx2dAM5Pf-ULeoMQ",
+  authDomain: "challenge-5a279.firebaseapp.com",
+  projectId: "challenge-5a279",
+  storageBucket: "challenge-5a279.appspot.com",
+  messagingSenderId: "487002200528",
+  appId: "1:487002200528:web:67e490cb6ad379c6fe01d5",
+};
